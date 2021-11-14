146f1acbccc041d1da0b6b8d8549048f461e9a5e
kodeah <kodeah@posteo.mx>
2021-11-14 19:03:28 +0100

    Add creation and setting of privileges for music download directory to debian install script

--- installDebian.sh
+++ installDebian.sh
@@ -28,4 +28,17 @@ sudo mkdir -p "$logDir"
 currentUserName="$(whoami)"
 sudo chown "$currentUserName:$currentUserName" "$logDir"
 
+# If mpd has default music directory configured, make it writable by all:
+if [ $(grep ^music_directory /etc/mpd.conf | grep -F "/var/lib/mpd/music" | wc -l) == 1 ]; then
+	echo "Mpd's music library seems to be configured to be at /var/lib/mpd/music. To create music download folder at /var/lib/mpd/music/dl and make it writable by all users (chmod 777), hit [Enter]. Otherwise, abort ([ctrl+c])."
+	read # Wait for user input
+	if [ ! -d /var/lib/mpd/music/dl ]; then
+		sudo mkdir /var/lib/mpd/music/dl
+		sudo chown mpd:audio /var/lib/mpd/music/dl
+	fi
+	sudo chmod 777 /var/lib/mpd/music/dl
+else
+	echo "Setup nearly finished. To make everything work, you should create a music download folder and make it writable by the user which you intend to run this program. For more information regarding the download folder, see the README file."
+fi
+
 echo "Finished."
