d6cdf4017fd996268c0fd8b548d9f86553212b91
kodeah <kodeah@posteo.mx>
2021-11-22 15:24:47 +0100

    Restructuring config

--- src/main/java/config/OwnerConfig.java
+++ src/main/java/config/OwnerConfig.java
@@ -9,19 +9,18 @@ public interface OwnerConfig extends Config {
     // Usage hints:
     // http://owner.aeonbits.org/docs/usage/
 
+    // Global configuration:
+    @DefaultValue("6646")
+    int portListen();
     @DefaultValue("/var/log/partyPartyPlaylist/p3Server.log")
     String logFilePath();
-
     @DefaultValue("/tmp")
     String tmpDirectoryPath();
 
+    // MPD specific configuration:
     @DefaultValue("/var/lib/mpd/music")
     String mpdMusicLibraryDirectoryPath();
-
     @DefaultValue("/var/lib/mpd/music/p3-cache")
     String mpdMusicDownloadDirectoryPath();
-
-    @DefaultValue("6646")
-    int portListen();
 
 }
